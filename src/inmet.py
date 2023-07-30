de8cb883e4177aad468584c33016ee2badaa5c27
Richard Barros <richardpuga2002@gmail.com>
2023-07-30 12:43:39 -0300

    :recycle: refactor - refatoração dos metodos

--- src/inmet.py
+++ src/inmet.py
@@ -2,6 +2,11 @@ import requests
 import json
 import urllib.parse
 
+_API_PREV3 = 'https://apiprevmet3.inmet.gov.br'
+_HEADERS = {
+    'Content-Type': 'application/json;charset=utf-8'
+}
+
 class inmet:
 
     """ 
@@ -9,74 +14,54 @@ class inmet:
         em seus próprios métodos de obtenção de dados através de consultas em APIs do próprio
         INMET (Instituto Nacional de Meteorologia)
 
-        CONSTANTS:
-        - _API_PREV3 : str -> URL da API interna de previsão meteorológica utilizada pelo site.
+        Atributos:
+        - `session: requests.Session`
 
-        - _HEADERS: str -> configuração padrão de cabeçalho para as requisições.
+        Métodos:
+        - `busca_cidade(city_name: str) -> list | dict`: Retorna informações sobre uma ou mais cidades a partir do nome fornecido.
+        - `previsao_por_capital() -> dict`: Retorna os dados de previsão do tempo atual e dos próximos 4 dias de todas as capitais do país.
+        - `previsao_por_cidade(geocode: int) -> dict`: Retorna os dados de previsão do tempo atual e dos próximos 4 dias de uma cidade específica com base no código de localização (geocode).
+        - `estacao_automatica(geocode: int) -> dict`: Retorna informações de uma estação meteorológica automática localizada em ou próximo da geolocalização do município informado, com base no código de localização (geocode).
 
-        ATTRIBUTES:
-        - session: requests -> sessão requests para efetuar as requisições
-
-        METHODS:
-        - busca_cidade: json -> retorna uma ou mais cidades.
-
-        - previsao_por_capital: json -> retorna os dados de previsão do tempo do dia atual e dos
-          próximos 4 dias de todas as capitais do país.
-
-        - previsao_por_cidade: json -> retorna os dados de previsão do tempo do dia atual e dos
-          próximos 4 dias de uma cidade específica.
     """
 
-
-    _API_PREV3 = 'https://apiprevmet3.inmet.gov.br'
-    _HEADERS = {
-        'Content-Type': 'application/json;charset=utf-8'
-    }
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.session = requests.Session()
 
-    def busca_cidade(
-        self,
-        nome_cidade: str
-    ) -> json:
+    def _parse_response(self, response: requests.Response) -> list | dict:
+        try:
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException:
+            return {
+                'status_code': response.status_code,
+                'reason': response.reason,
+                'content': response.text
+            }
+
+    def busca_cidade(self, nome_cidade: str) -> list | dict:
         
         """
-            Retorna uma ou mais cidades.
+            Retorna informações sobre uma ou mais cidades a partir do nome fornecido.
 
             PARAMS:
             - nome_cidade: str -> nome da cidade para busca.
         """
+        url = f'{_API_PREV3}/autocomplete/{urllib.parse.quote(nome_cidade)}'
+        response = self.session.get(url, headers=_HEADERS)
+        return self._parse_response(response)
 
-        response = self.session.get(self._API_PREV3 + '/autocomplete/%s' % urllib.parse.quote(nome_cidade), headers=self._HEADERS)
-
-        return json.loads(response.text) if response.status_code == 200 else  json.loads({
-            'status_code': response.status_code,
-            'reason': response.reason,
-            'content': response.text
-        })
-
-    def previsao_por_capital(
-        self,
-    ) -> json:
+    def previsao_por_capital(self) -> dict:
 
         """
-            Retorna os dados de previsão do tempo do dia atual e dos
+            Retorna os dados de previsão do tempo atual e dos
             próximos 4 dias de todas as capitais do país
         """
+        url = f'{_API_PREV3}/previsao/capitais'
+        response = self.session.get(url, headers=_HEADERS)
+        return self._parse_response(response)
 
-        response = self.session.get(self._API_PREV3 + '/previsao/capitais',  headers=self._HEADERS)
-
-        return json.loads(response.text) if response.status_code == 200 else json.loads({
-            'status_code': response.status_code,
-            'reason': response.reason,
-            'content': response.text
-        })
-
-    def previsao_por_cidade(
-        self,
-        geocode: int
-    ) -> json:
+    def previsao_por_cidade(self, geocode: int) -> dict:
         
         """
             Retorna os dados de previsão do tempo do dia atual e dos próximos 4 dias de uma cidade específica. 
@@ -84,11 +69,19 @@ class inmet:
             PARAMS:
             - geocode: int -> código de localização da cidade encontrada no objeto retornado pelo método 'busca_cidade'.
         """
+        url = f'{_API_PREV3}/previsao/{geocode}'
+        response = self.session.get(url, headers=_HEADERS)
+        return self._parse_response(response)
+    
+    def estacao_proxima(self, geocode: int) -> dict:
         
-        response = self.session.get(self._API_PREV3 + '/previsao/%i' % geocode)
+        """
+            Retorna dados de uma estação meteorológica automática
+            localizada em um ponto fixo na ou próximo da geolocalização do município informado
 
-        return json.loads(response.text) if response.status_code == 200 else json.loads({
-            'status_code': response.status_code,
-            'reason': response.reason,
-            'content': response.text
-        })
+            PARAMS:
+            - geocode: int -> código de localização da cidade encontrada no objeto retornado pelo método 'busca_cidade'.
+        """
+        url = f'{_API_PREV3}/estacao/proxima/{geocode}'
+        reponse = self.session.get(url, headers=_HEADERS)
+        return self._parse_response(reponse)
